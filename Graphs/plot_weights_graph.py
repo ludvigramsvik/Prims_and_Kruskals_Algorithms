c91e8b5ae89987cf2ee31effe73dc91768591e6d
ludvigramsvik <78752700+ludvigramsvik@users.noreply.github.com>
2026-05-07 12:46:54 +0200

    fd

--- Graphs/plot_weights_graph.py
+++ Graphs/plot_weights_graph.py
@@ -3,18 +3,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 class PlotWeightsGraph:
-    def __init__(self, graph):
+    def __init__(self, graph, random_seed=42):
         self.graph = graph
 
-        np.random.seed(42)
+        np.random.seed(random_seed)
         pos = nx.spring_layout(self.graph, k=0.8, iterations=100)
-    
-        # 3. Draw with slightly smaller nodes and fonts
+
         nx.draw(self.graph, pos, 
                 with_labels=True, 
                 node_color='lightblue', 
                 edge_color='gray')
-    
+
         edge_labels = nx.get_edge_attributes(self.graph, 'weight')
         nx.draw_networkx_edge_labels(self.graph, pos, edge_labels=edge_labels, font_size=7, bbox=dict(facecolor='white', edgecolor='none', alpha=1))
         plt.show()
